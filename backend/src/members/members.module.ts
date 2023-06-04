dae18c086b1a3beecfbcee38406e3b100adefbf2
mohamed MASSTOUR <mmasstou@student.1337.ma>
2023-06-04 18:44:18 +0100

    members is done

--- backend/src/members/members.module.ts
+++ backend/src/members/members.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/users/user.service';
 
 @Module({
-  providers: [MembersService],
+  providers: [MembersService, PrismaService, UserService],
   controllers: [MembersController],
 })
 export class MembersModule {}
--- backend/src/members/members.service.ts
+++ backend/src/members/members.service.ts
@@ -1,4 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Members, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class MembersService {}
+export class MembersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(params: { id: string }): Promise<Members> {
+    try {
+      const { id } = params;
+      console.log('++findOne||++>', id);
+      const message = await this.prisma.members.findUnique({
+        where: { id },
+      });
+      if (!message) throw new Error('');
+      return message;
+    } catch (error) {
+      throw new NotFoundException();
+    }
+  }
+
+  async findAll(): Promise<Members[]> {
+    return this.prisma.members.findMany();
+  }
+
+  async create(data: {
+    type: any | Prisma.EnumUserTypeFieldUpdateOperationsInput;
+    user: string;
+    roomId: string;
+  }): Promise<Members> {
+    // const user =
+    try {
+      const { type, user, roomId } = data;
+      return await this.prisma.members.create({
+        data: {
+          type: type,
+          user: {
+            connect: { id: user },
+          },
+          RoomId: {
+            connect: { id: roomId },
+          },
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: "CAN'T CREATE THIS MESSAGE",
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
+  }
+
+  async update(params: {
+    id: string;
+    type: Prisma.EnumUserTypeFieldUpdateOperationsInput;
+  }): Promise<Members> {
+    const { id, type } = params;
+    console.log('++update++>', id);
+
+    return await this.prisma.members.update({
+      data: { type },
+      where: { id },
+    });
+  }
+
+  async remove(id: string): Promise<Members> {
+    console.log('++remove++>', id);
+
+    return await this.prisma.members.delete({
+      where: { id },
+    });
+  }
+}
