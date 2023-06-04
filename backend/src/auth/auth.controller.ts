938338002e748f77e6e76348ceedb39d425d50ac
Mohamed Masstour <mmasstou@e2r9p12.1337.ma>
2023-06-04 23:11:49 +0100

    add: Adding joinUser and config Dockerfile for database and backend

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
--- backend/src/rooms/rooms.module.ts
+++ backend/src/rooms/rooms.module.ts
@@ -3,9 +3,10 @@ import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/users/user.service';
+import { MembersService } from 'src/members/members.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService, PrismaService, UserService],
+  providers: [RoomsService, PrismaService, UserService, MembersService],
 })
 export class RoomsModule {}
