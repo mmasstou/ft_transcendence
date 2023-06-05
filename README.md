73717294a4f7274b21457d133f0c64b53a99457a
Azedine ouhadou <aouhadou@student.1337.ma>
2023-06-05 08:29:12 +0100

    add circle animation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ft_transcendence
+absolute border border-[#c3c3c3] rounded-full gl:h-[800px] gl:w-[800px] xl:h-[800px] xl:w-[800px]
+             xl:mb-[450px] lg:mb-[450px]  h-[650px] w-[650px] gl:ml-[900px] xl:ml-[900px]  mt-[600px] gl:-mt-0"
--- front/src/components/BackgroundCircles.tsx
+++ front/src/components/BackgroundCircles.tsx
@@ -11,21 +11,30 @@ const BackgroundCircles = () => {
         opacity: 0,
       }}
       animate={{
-        scale: [1, 1, 2, 3, 1],
+        scale: [0.5, 0.8, 1, 1, 0.8],
         opacity: [0.1, 0.2, 0.4, 0.8, 0.6, 0.4],
         borderRadius: ["20%", "20%", "50%", "80%", "20%",]
       }}
       transition={{
         duration: 2.5,
-      }}
-      className="relative flex justify-center items-center">
+      }} 
+      className="relative flex justify-center items-center md:flex-col sm:flex-col  sm:mt-50">
 
-      <div className=" absolute border border-tertiary rounded-full h-[200px] w-[200px] 
-            ml-[700px] animate-ping" />
-      <div className="absolute border border-tertiary rounded-full h-[300px] w-[300px] ml-[700px]  " />
-      <div className="absolute border border-tertiary rounded-full h-[500px] w-[500px] ml-[700px]" />
-      <div className="absolute border border-secondary opacity-20 rounded-full h-[650px] w-[650px] ml-[700px] animate-pulse" />
-      <div className="absolute border border-tertiary rounded-full h-[800px] w-[800px] ml-[700px]" />
+      <div className=" absolute border border-[#c3c3c3] rounded-full h-[170px] w-[170px] lg:h-[200px] lg:w-[200px] 
+            xl:h-[200px] xl:w-[200px] lg:ml-[750px] xl:ml-[900px] md:mt-[500px]  mt-[600px] lg:mt-[500px]  xl:mb-[450px] lg:mb-[450px]" />
+
+      <div className="absolute border border-[#c3c3c3] rounded-full h-[220px] w-[220px] lg:h-[300px] lg:w-[300px] 
+            xl:h-[300px] xl:w-[300px] lg:ml-[750px] xl:ml-[900px] animate-ping  mt-[600px] lg:mt-[500px]  xl:mb-[450px] lg:mb-[450px]" />
+
+      <div className="absolute border border-[#c3c3c3] rounded-full h-[420px] w-[420px] lg:h-[500px] lg:w-[500px] 
+            xl:h-[500px] xl:w-[500px] lg:ml-[750px] xl:ml-[900px]  mt-[600px] lg:mt-[500px]  xl:mb-[450px] lg:mb-[450px]" />
+
+      <div className="absolute border border-secondary opacity-20 rounded-full lg:h-[650px] lg:w-[650px] 
+            xl:h-[650px] xl:w-[650px] h-[570px] w-[570px] lg:ml-[750px] xl:ml-[900px]  mt-[600px] lg:mt-[500px]
+            xl:mb-[450px] lg:mb-[450px]  animate-pulse" />
+
+      <div className="absolute border border-[#c3c3c3] rounded-full lg:h-[800px] lg:w-[800px] xl:h-[800px] xl:w-[800px]
+             xl:mb-[450px] lg:mb-[450px]  h-[650px] w-[650px] lg:ml-[750px] xl:ml-[900px]  mt-[600px] lg:mt-[500px] " />
     </motion.div>
   )
 }
