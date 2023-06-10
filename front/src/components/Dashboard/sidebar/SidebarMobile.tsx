323b994a5cfec06f740297c6731577cdfed47fc5
mohamed MASSTOUR <mmasstou@student.1337.ma>
2023-06-10 11:05:46 +0100

    fix dashboard styles

--- front/src/components/Dashboard/sidebar/SidebarMobile.tsx
+++ front/src/components/Dashboard/sidebar/SidebarMobile.tsx
@@ -10,15 +10,13 @@ const SidebarMobile = () => {
     const router = usePathname();
   
       return (
-        <nav className="fixed bottom-0 bg-[#243230] h-[5rem] w-full px-6 rounded-t-xl md:hidden">
-            <div className="relative top-3 h-full ">
-                <ul className="flex flex-row justify-around items-center">
+        <nav className="fixed bottom-0 bg-[#243230] h-[3rem] w-full px-6 py-4 rounded-t-xl md:hidden ">
+                <ul className="relative h-full flex flex-row justify-around items-center">
                     <MenuItem isActive={router === '/profile'} href='/profile' icon={CgProfile}/>
                     <MenuItem isActive={router === '/chat'} href='/chat' icon={AiFillWechat}/>
                     <MenuItem isActive={router === '/users'} href='/users' icon={FaUsers}/>
                     <MenuItem isActive={router === '/game'} href='/game' icon={IoLogoGameControllerB}/>
                 </ul>
-            </div>
         </nav>
       );
 }
