bca768ff7bda20e55c63b91e03131a92559cc859
mohamed MASSTOUR <mmasstou@student.1337.ma>
2023-07-17 21:16:16 +0100

    add: Adding select component for conves

---
+++ front/src/modals/Select.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+import ReactSelect from "react-select";
+interface SelectProps {
+    lable: string;
+    value?: Record<string, any>;
+    onChange?: (value: Record<string, any>) => void;
+    options: Record<string, any>[];
+    isMulti?: boolean;
+    disabled?: boolean;
+}
+const Select: FC<SelectProps> = ({ lable, value, onChange, options, isMulti, disabled }) => {
+    const customStyles = {
+        multiValue: (provided: any, state: any) => ({
+            ...provided,
+            color: 'white',
+            padding: '0.5rem',
+            
+
+        }),
+        multiValueRemove: (provided: any, state: any) => ({
+            ...provided,
+            backgroundColor: 'gray-300',
+            color: 'red',
+            borderRadius: '9999px',
+            ':hover': {
+              backgroundColor: 'transparent'
+            }
+          }),
+        control: (provided: any, state: any) => ({
+            ...provided,
+            backgroundColor: 'transparent',
+            padding: state.isSelected ? '0.2rem' : '0.5rem',
+            border: '1px solid #E0E0E0',
+
+        }),
+        menuPortal: (base: any) => ({ ...base, zIndex: 9999 })
+    };
+    return (
+        <div className="z-[100]">
+            <h1 className="text-slate-600 text-xl font-bold capitalize">{lable}</h1>
+            <div className="mt-2 max-h-[5vh] overflow-y-scroll">
+                <ReactSelect
+                    isDisabled={disabled}
+                    value={value}
+                    onChange={onChange}
+                    isMulti
+                    options={options}
+                    menuPortalTarget={document.body}
+                    styles={customStyles}
+                    classNames={{
+                        control: () => "text-sm"
+                    }}
+                />
+            </div>
+        </div>
+    )
+}
+export default Select;
