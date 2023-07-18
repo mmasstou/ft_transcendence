4103c9940f36753973296314cfa60690089e37fd
shonjmoj <midoounejjart@gmail.com>
2023-07-18 09:52:50 +0100

    feat: add game and game modes pages

--- front/src/app/game/page.tsx
+++ front/src/app/game/page.tsx
@@ -1,17 +1,16 @@
-'use client';
 import Image from 'next/image';
 import Dashboard from '../Dashboard';
-import React from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
-import { Cross2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 const page = () => {
   return (
     <Dashboard>
-      <div className="w-full flex flex-col gap-10 items-center p-4 text-left ">
-        <h1 className="text-2xl xl:text-4xl font-bold">Game Play</h1>
+      <div className="w-full flex flex-col gap-10 items-center p-4 text-left tracking-wide">
+        <h1 className="text-2xl xl:text-4xl 2xl:text-5xl font-bold">
+          Game Play
+        </h1>
         <div className="flex flex-col lg:flex-row-reverse gap-6 lg:mt-8 w-full items-center justify-center px-4">
-          <div className="flex flex-col w-full items-center gap-4 lg:gap-8 leading-relaxed xl:text-lg">
+          <div className="flex flex-col w-full items-center gap-4 lg:gap-10 xl:gap-16 leading-relaxed lg:text-lg xl:text-xl">
             <p>
               Welcome to our ping pong play page! Get ready to experience the
               thrill of virtual ping pong right from the comfort of your own
@@ -26,34 +25,21 @@ const page = () => {
               to enter our vibrant community and engage in exhilarating matches.
             </p>
             <div className="flex gap-10 my-4 w-full justify-center">
-              <button className="px-4 py-1 border border-yellow-500 rounded-xl text-yellow-500">
-                Practice Mode
-              </button>
-              <Dialog.Root>
-                <Dialog.Trigger asChild>
-                  <button className="px-4 py-1 border border-secondary rounded-xl text-secondary focus:outline-none">
-                    Multiplayer Mode
-                  </button>
-                </Dialog.Trigger>
-                <Dialog.Portal>
-                  <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0" />
-                  <Dialog.Content
-                    className="data-[state=open]:animate-contentShow text-white rounded-lg bg-[#243230] p-6 fixed top-[25%] left-[50%] max-h-full w-[90vw] max-w-[800px] translate-x-[-50%] lg:translate-x-[-45%] xl:translate-x-[-35%] translate-y-[-50%] 
-                  shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]
-                  focus:outline-none"
-                  >
-                    <Dialog.Title className="">Invite a friend !</Dialog.Title>
-                    <Dialog.Close asChild>
-                      <button className="text-white top-5 right-5 absolute">
-                        <Cross2Icon />
-                      </button>
-                    </Dialog.Close>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root>
+              <Link
+                href={'/game/modes'}
+                className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2 border-yellow-500 rounded-xl font-bold text-yellow-500"
+              >
+                Time Mode
+              </Link>
+              <Link
+                href={'/game/modes'}
+                className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2  border-secondary rounded-xl font-bold text-secondary focus:outline-none"
+              >
+                Score Mode
+              </Link>
             </div>
           </div>
-          <div className="relative h-96 w-full">
+          <div className="relative h-[400px] md:h-[500px] xl:h-[600px] w-full">
             <Image fill src="/game-play 1.svg" alt="game-play" className="" />
           </div>
         </div>
--- front/src/components/Dashboard/DesktopSidebar.tsx
+++ front/src/components/Dashboard/DesktopSidebar.tsx
@@ -1,24 +1,36 @@
-"use client"
-import { usePathname } from "next/navigation"
-import {CgProfile} from 'react-icons/cg';
-import {AiFillWechat} from 'react-icons/ai';
-import {FaUsers} from 'react-icons/fa';
-import {IoLogoGameControllerB} from 'react-icons/io';
-import MenuItem from "./sidebar/MenuItem"
+'use client';
+import { usePathname } from 'next/navigation';
+import { CgProfile } from 'react-icons/cg';
+import { AiFillWechat } from 'react-icons/ai';
+import { FaUsers } from 'react-icons/fa';
+import { IoLogoGameControllerB } from 'react-icons/io';
+import MenuItem from './sidebar/MenuItem';
 
 const DesktopSidebar = () => {
-    const router = usePathname();
+  const router = usePathname();
 
   return (
-      <nav className="flex flex-col justify-between items-center h-full p-1">
-            <ul className="flex flex-col justify-center items-center gap-12 w-full h-full">
-                <MenuItem isActive={router === '/profile'} href='/profile' icon={CgProfile}/>
-                <MenuItem isActive={router === '/chat'} href='/chat' icon={AiFillWechat}/>
-                <MenuItem isActive={router === '/users'} href='/users' icon={FaUsers}/>
-                <MenuItem isActive={router === '/game'} href='/game' icon={IoLogoGameControllerB}/>
-            </ul>
-        </nav>
-  )
-}
+    <nav className="flex flex-col justify-between items-center h-full p-1">
+      <ul className="flex flex-col justify-center items-center gap-12 w-full h-full">
+        <MenuItem
+          isActive={router === '/profile'}
+          href="/profile"
+          icon={CgProfile}
+        />
+        <MenuItem
+          isActive={router === '/chat'}
+          href="/chat"
+          icon={AiFillWechat}
+        />
+        <MenuItem isActive={router === '/users'} href="/users" icon={FaUsers} />
+        <MenuItem
+          isActive={router === '/game' || router === '/game/modes'}
+          href={'/game' || '/game/modes'}
+          icon={IoLogoGameControllerB}
+        />
+      </ul>
+    </nav>
+  );
+};
 
-export default DesktopSidebar
+export default DesktopSidebar;
