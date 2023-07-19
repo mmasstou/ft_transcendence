57f0266f7af5876c518f3daeea277757b2e9cc7d
mohamed MASSTOUR <mmasstou@student.1337.ma>
2023-07-19 09:47:16 +0100

    add: ChanneL nav bar finish

--- front/src/components/chat/chat.userAvater.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image"
-
-export function UserAvatar({ image, size }: { image: string; size?: number; }) {
-    const _size = size ? size : 28
-    return <div className={`image  min-w-[${_size}px] rounded-full overflow-hidden`}>
-        <Image src={image} alt={"avatar"} width={_size} height={_size} />
-    </div>
-}
