0d05652c466e97e7255ebbf3178f13808f2a3e47
shonjmoj <midoounejjart@gmail.com>
2023-07-25 14:36:58 +0100

    fix: user card status

--- front/src/components/ui/user/UserCard.tsx
+++ front/src/components/ui/user/UserCard.tsx
@@ -8,7 +8,6 @@ const UserCard: FC<UserCardProps> = ({
   addRequest,
   online,
   inGame,
-  invite,
 }) => {
   return (
     <div className=" bg-container rounded-xl my-3 p-2 xl:p-3 flex items-center justify-between">
@@ -17,44 +16,55 @@ const UserCard: FC<UserCardProps> = ({
         <h3 className="xl:text-lg">{username}</h3>
       </div>
       <div className="flex items-center text-xs xl:text-sm gap-2">
-        {addRequest || invite ? (
+        {addRequest ? (
           <button className="flex items-center border p-1 px-2 xl:px-3 rounded-xl border-sky-500 text-sky-500 hover:bg-sky-600 hover:text-container hover:border-container group transition-colors">
-            {addRequest ? (
-              <TiUserAdd
-                className="mr-1 fill-sky-500 group-hover:fill-container"
-                size={16}
-              />
-            ) : (
-              invite && (
-                <BiJoystick
+            <TiUserAdd
+              className="mr-1 fill-sky-500 group-hover:fill-container"
+              size={16}
+            />
+            Add Friend
+          </button>
+        ) : (
+          online && (
+            <button className="flex items-center border p-1 px-2 xl:px-3 rounded-xl border-sky-500 text-sky-500 hover:bg-sky-600 hover:text-container hover:border-container group transition-colors">
+              {addRequest ? (
+                <TiUserAdd
                   className="mr-1 fill-sky-500 group-hover:fill-container"
                   size={16}
                 />
-              )
-            )}
-            {addRequest ? 'Add Friend' : invite && 'Invite'}
-          </button>
-        ) : null}
-        <div
-          className={`border flex gap-1 items-center p-1 px-2 xl:px-3 rounded-xl ${
-            inGame
-              ? 'border-orange-500'
-              : online
-              ? 'border-green-500'
-              : 'border-yellow-500'
-          }`}
-        >
+              ) : (
+                online && (
+                  <BiJoystick
+                    className="mr-1 fill-sky-500 group-hover:fill-container"
+                    size={16}
+                  />
+                )
+              )}
+              Invite
+            </button>
+          )
+        )}
+        {!addRequest && (
           <div
-            className={`w-2 h-2 rounded-full ${
+            className={`border flex gap-1 items-center p-1 px-2 xl:px-3 rounded-xl ${
               inGame
-                ? 'bg-orange-500'
+                ? 'border-orange-500'
                 : online
-                ? 'bg-green-500'
-                : 'bg-yellow-500'
+                ? 'border-green-500'
+                : 'border-yellow-500'
             }`}
-          ></div>
-          <span
-            className={`
+          >
+            <div
+              className={`w-2 h-2 rounded-full ${
+                inGame
+                  ? 'bg-orange-500'
+                  : online
+                  ? 'bg-green-500'
+                  : 'bg-yellow-500'
+              }`}
+            ></div>
+            <span
+              className={`
             ${
               inGame
                 ? 'text-orange-500'
@@ -63,10 +73,11 @@ const UserCard: FC<UserCardProps> = ({
                 : 'text-yellow-500'
             }
           `}
-          >
-            {inGame ? 'In Game' : online && !inGame ? 'Online' : 'Offline'}
-          </span>
-        </div>
+            >
+              {inGame ? 'In Game' : online && !inGame ? 'Online' : 'Offline'}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
--- front/src/types/UserCardTypes.ts
+++ front/src/types/UserCardTypes.ts
@@ -1,8 +1,7 @@
 export interface UserCardProps {
-  inGame?: boolean;
   username: string;
   avatar?: string;
   addRequest?: boolean;
-  invite?: boolean;
   online?: boolean;
+  inGame?: boolean;
 }
