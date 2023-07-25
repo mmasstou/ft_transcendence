1af99f647ad78558a53d184c0cbb63ec79f09942
mohamed MASSTOUR <mmasstou@student.1337.ma>
2023-07-25 04:05:55 +0100

    update: Updateing settings.user.item

--- front/src/app/chat/channels/modaLs/channel.settings.edit.modal.tsx
+++ front/src/app/chat/channels/modaLs/channel.settings.edit.modal.tsx
@@ -4,9 +4,11 @@ import React, { MouseEvent, useEffect, useState } from "react"
 interface ChanneLSettingsEditModaLProps {
     children: React.ReactNode,
 }
-const ChanneLSettingsEditModaL = ( {children} : ChanneLSettingsEditModaLProps) => {
+const ChanneLSettingsEditModaL = ({ children }: ChanneLSettingsEditModaLProps) => {
     const [Ismounted, setIsmounted] = useState<boolean>(false)
-   return <div className='flex flex-col gap-4'>{children}</div>
+    useEffect(() => { setIsmounted(true) }, [])
+    if (!Ismounted) return null
+    return <div className='flex flex-col gap-4'>{children}</div>
 
 }
 export default ChanneLSettingsEditModaL
