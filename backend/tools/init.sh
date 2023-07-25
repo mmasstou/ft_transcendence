2ae6e26dbd62be72644fc32b07e3a13cd389cc89
mmasstou <masstour01@gmail.com>
2023-07-25 03:40:06 +0100

    fixing docker compose errors

--- backend/tools/init.sh
+++ backend/tools/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-sleep 10
+sleep 12
+echo "  ++> Waiting for postgres..."
 npx prisma migrate dev --name init 
-
+echo "  ++> PostgreSQL started"
 # npx prisma generate
 # npx ts-node script.ts
 
-npm run start:dev
+
 
 # tail -f
 
