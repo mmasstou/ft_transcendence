545665134819697c2ccb9b1b55d5c43539301e72
marbenMB <mbenbajj@student.1337.ma>
2023-08-13 22:57:14 +0100

    Start Frontend

--- front/src/app/chat/directmessage/page.tsx
+++ front/src/app/chat/directmessage/page.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Dashboard from '@/app/Dashboard';
+import { HiChatBubbleLeftRight } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
+import { FaUsers } from "react-icons/fa";
+import ChatNavbarLink from '../components/chat.navbar.link';
+import Button from '../components/Button';
+import { BsReverseLayoutSidebarInsetReverse } from 'react-icons/bs';
+
 
 const metadata = {
   title: 'Transcendence',
   description: 'Online Pong Game',
 };
 export default function page() {
+    const router = usePathname();
+	const [isOpen, setOpening] = useState(false);
+
   return (
     <Dashboard>
-     Direct Message
+    <div className='h-full border-4 border-[#24323044]'>
+		<div className="channeLnavbarmenu col-span-2 flex justify-center sm:justify-around gap-4 w-full">
+				<div>
+					<Button
+						{isOpen ? icon={BsLayoutSidebarInset} : icon={BsReverseLayoutSidebarInsetReverse}}
+						small
+						outline
+						onClick={() => setOpening(true)}
+					/>
+				</div>
+				<ChatNavbarLink
+					to="/chat/directmessage"
+					label="direct Message"
+					icon={HiChatBubbleLeftRight}
+					active={router.includes('directmessage')}
+				/>
+				<ChatNavbarLink
+					to="/chat/channels"
+					label="channeL"
+					icon={FaUsers}
+					active={router.includes('channels')}
+				/>
+		</div>
+	</div>
     </Dashboard>
   )
 }
--- front/src/app/chat/page.tsx
+++ front/src/app/chat/page.tsx
@@ -1,14 +1,37 @@
+'use client';
 import React from 'react';
 import Dashboard from '../Dashboard';
+import ChatNavbarLink from './components/chat.navbar.link';
+import { HiChatBubbleLeftRight } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
+import { FaUsers } from "react-icons/fa";
+
 
 const metadata = {
   title: 'Transcendence',
   description: 'Online Pong Game',
 };
 export default function page() {
+    const router = usePathname();
+
   return (
     <>
-      <Dashboard>chat</Dashboard>
+      <Dashboard>
+		<div className=" h-full channeLnavbarmenu col-span-2 flex justify-center items-center border-2 sm:justify-around gap-4 w-full ">
+			<ChatNavbarLink
+				to="/chat/directmessage"
+				label="direct Message"
+				icon={HiChatBubbleLeftRight}
+				active={router.includes('directmessage')}
+			/>
+			<ChatNavbarLink
+				to="/chat/channels"
+				label="channeL"
+				icon={FaUsers}
+				active={router.includes('channels')}
+			/>
+        </div>
+		</Dashboard>
     </>
   );
 }
