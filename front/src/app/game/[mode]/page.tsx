26c347f3dda5f93fb16d138b4508ebc6a85531ce
Ali Boulhajat <aboulhaj@e2r10p12.1337.ma>
2023-08-13 08:17:09 +0100

    update loading component, game ready to test

--- front/src/app/game/[mode]/page.tsx
+++ front/src/app/game/[mode]/page.tsx
@@ -55,44 +55,17 @@ const page = ({ params }: { params: { mode: string } }) => {
               }}>
                 Robot
               </button>
-              {/* <Dialog.Root>
-                <Dialog.Trigger asChild> */}
-                  <button className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2 border-secondary rounded-xl font-bold text-secondary" onClick={() => {
-                    axios.post('http://127.0.0.1:80/api/game/RandomGame', body).then((res) => {
-                      router.push(`/game/${params.mode}/random`)
-                    })
-                    .catch((err) => {
-                      router.push('/game');
-                      toast.error(err.response.data.reason);
-                    });
-                  }}>
-                    Random
-                  </button>
-                {/* </Dialog.Trigger>
-              <Dialog.Portal>
-                  <Dialog.Overlay
-                    className="bg-black opacity-60 data-[state=open]:animate-overlayShow fixed inset-0"
-                    onPointerDown={(e) => e.stopPropagation()}
-                  />
-                  <Dialog.Content
-                    className="flex flex-col items-center gap-6 lg:gap-8 xl:gap-10 data-[state=open]:animate-contentShow text-white rounded-lg bg-[#243230] p-6 fixed top-1/2 left-1/2 max-h-full w-[80vw] md:w-[50vw] xl:w-[30vw] translate-x-[-50%] translate-y-[-50%] 
-                  shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]
-                  focus:outline-none"
-                  >
-                    <Dialog.Title>
-                      <h1 className="text-2xl md:text-3xl xl:text-4xl tracking-wider">
-                        Matchmaking Queue
-                      </h1>
-                    </Dialog.Title>
-                    <PiSpinnerGap className="animate-spin w-14 h-14 xl:w-20 xl:h-20 fill-secondary" />
-                    <Dialog.Close asChild>
-                      <button className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2  border-danger rounded-xl font-bold text-danger focus:outline-none">
-                        Cancel
-                      </button>
-                    </Dialog.Close>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root> */}
+              <button className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2 border-secondary rounded-xl font-bold text-secondary" onClick={() => {
+                axios.post('http://127.0.0.1:80/api/game/RandomGame', body).then((res) => {
+                  router.push(`/game/${params.mode}/random`)
+                })
+                .catch((err) => {
+                  router.push('/game');
+                  toast.error(err.response.data.reason);
+                });
+              }}>
+                Random
+              </button>
               <Dialog.Root>
                 <Dialog.Trigger asChild>
                   <button className="px-4 py-1 xl:px-6 xl:py-2 border xl:border-2  border-orange-500 rounded-xl font-bold text-orange-500 focus:outline-none">
