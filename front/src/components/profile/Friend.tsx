f3c5c733bd83f8e8cc26810f20a9c964056fa76e
mmasstou <masstour01@gmail.com>
2023-08-30 11:12:12 +0100

    me: add avatar to Friend.tsx for build

--- front/src/components/profile/Friend.tsx
+++ front/src/components/profile/Friend.tsx
@@ -5,9 +5,9 @@ import FriendCard from './FriendCard';
 const Friend = () => {
   return (
     <div className="bg-[#243230] rounded-md text-white flex flex-col p-2 xl:p-3 overflow-auto">
-      <FriendCard username="mehdi" />
-      <FriendCard username="mehdi" inGame />
-      <FriendCard username="mehdi" online />
+      <FriendCard avatar='' username="mehdi" />
+      <FriendCard avatar='' username="mehdi" inGame />
+      <FriendCard avatar='' username="mehdi" online />
     </div>
   );
 };
