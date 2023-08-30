5d1f361cae8ed2d215ead4a3e31cf8e3f4129a2d
ALI-BOULHAJAT <ali.boulhajat@gamil.com>
2023-08-30 18:41:41 +0100

    update deny invite from socket, and disabling button

--- front/src/components/profile/Friend.tsx
+++ front/src/components/profile/Friend.tsx
@@ -5,9 +5,9 @@ import FriendCard from './FriendCard';
 const Friend = () => {
   return (
     <div className="bg-[#243230] rounded-md text-white flex flex-col p-2 xl:p-3 overflow-auto">
-      <FriendCard username="mehdi" avatar={''} userId={''} socket={undefined} mode={''} />
-      <FriendCard username="mehdi" inGame avatar={''} userId={''} socket={undefined} mode={''} />
-      <FriendCard username="mehdi" online avatar={''} userId={''} socket={undefined} mode={''} />
+      <FriendCard username="mehdi" status="online" avatar={''} userId={''} socket={undefined} mode={''} />
+      <FriendCard username="mehdi" status="inGame" avatar={''} userId={''} socket={undefined} mode={''} />
+      <FriendCard username="mehdi" status="offline" avatar={''} userId={''} socket={undefined} mode={''} />
     </div>
   );
 };
